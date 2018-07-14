525d1ec15e28e655a65ea02b85bb6c4c63ba534a
horance <horance@aliyun.com>
2018-07-14 21:52:29 +0800

    merge three test cases into one

--- test/mars/TestCaseSpec.cc
+++ test/mars/TestCaseSpec.cc
@@ -2,18 +2,14 @@
 #include <mars/core/TestMethod.h>
 
 namespace {
-  bool wasRun = false;
-  bool wasSetUp = false;
   std::string result;
 
   struct WasRun {
     void setUp() {
-      wasSetUp = true;
       result += "[setUp]";
     }
 
     void testMethod() {
-      wasRun = true;
       result += "[runTest]";
     }
   };
@@ -21,26 +17,12 @@ namespace {
   struct TestCaseSpec : testing::Test {
   private:
     void SetUp() override {
-      wasRun = false;
-      wasSetUp = false;
       result.clear();
     }
 
   protected:
     TestMethod<WasRun> test = &WasRun::testMethod;
   };
-}
-
-TEST_F(TestCaseSpec, make_sure_test_method_was_ran) {
-  ASSERT_FALSE(wasRun);
-  test.run();
-  ASSERT_TRUE(wasRun);
-}
-
-TEST_F(TestCaseSpec, should_setup_before_run_test) {
-  ASSERT_FALSE(wasSetUp);
-  test.run();
-  ASSERT_TRUE(wasSetUp);
 }
 
 TEST_F(TestCaseSpec, make_sure_setup_before_run_test) {
